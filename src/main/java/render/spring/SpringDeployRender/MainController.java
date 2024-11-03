2da01e162c5e6278daad1976af0ada7f426a7c83
Dima <barth0l0mew@mail.ru>
2024-11-03 21:46:40 +0300

    Create spring web service 6 update index

--- src/main/java/render/spring/SpringDeployRender/MainController.java
+++ src/main/java/render/spring/SpringDeployRender/MainController.java
@@ -1,12 +1,17 @@
 package render.spring.SpringDeployRender;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class MainController {
     @GetMapping({"","/","/index"})
-    public String index() {
+    public String index(Model model) {
+        System.out.println(LocalDateTime.now());
+        model.addAttribute("date", LocalDateTime.now());
         return "index";
     }
 }
